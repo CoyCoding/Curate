1485537952e7de2c7b91b25e25aee814cb98cdc9
Coycoding <fluffytoycoy@gmail.com>
2020-10-12 06:41:03 -0500

    404 fix

--- curate_front/src/views/404/NotFound.js
+++ curate_front/src/views/404/NotFound.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import './404.css';
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  bg:{
+    display: 'flex',
+    flexGrow: '1',
+    background: 'linear-gradient(45deg, #00000073, #0a0a0a63), url("./images/travolta.gif")',
+    backgroundSize: 'cover',
+    backgroundPosition: 'top'
+  },
+  code:{
+    fontSize: '15rem',
+    color: 'white',
+    margin: '48px'
+  }
+}));
+
 function NotFound(props){
+  const classes = useStyles();
   return (
     <>
-        <div className="bg" style={{ backgroundImage: 'linear-gradient(45deg, #00000073, #0a0a0a63), url("./images/travolta.gif")'}}><div className="code">404</div></div>
-
+        <div className={classes.bg}>
+          <div className={classes.code}>404</div>
+        </div>
     </>
   )
 
