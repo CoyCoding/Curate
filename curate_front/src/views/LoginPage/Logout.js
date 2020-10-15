9148dbad2f72295f8921165d6cce89d6faeb4169
Coycoding <fluffytoycoy@gmail.com>
2020-10-14 20:41:22 -0500

    pushing out temp auth header changes

--- curate_front/src/views/LoginPage/Logout.js
+++ curate_front/src/views/LoginPage/Logout.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
+import axios from 'axios';
+import { authHeaders } from './../../utils/authHeaders';
 
 const Logout = (props) => {
-  localStorage.removeItem('access-token');
-  props.setLoggedIn(false);
-  return ( <Redirect to="/"/>);
+  axios.post('https://curate.v1.coycoding.com/Logout',        {headers: {
+            "Authorization" : `Bearer ${localStorage.getItem('access-token')}`,
+        }})
+      .then(function (response) {
+        console.log(response)
+        localStorage.removeItem('access-token');
+        props.setLoggedIn(null);
+        return ( <Redirect to="/"/>);
+      })
+      .catch((error) => {
+        console.log(error);
+        localStorage.removeItem('access-token');
+          props.setLoggedIn(false);
+          props.setLoggedIn(null);
+          return ( <Redirect to="/"/>);
+  });
+
 }
 
 export default Logout;
